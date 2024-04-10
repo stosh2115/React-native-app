fc85b161467cbd966b6f7f7290d16fde0089995e
stosh2115 <stosh09@hotmail.com>
2024-04-10 15:03:20 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course Goal!'/>
+        <Button title='Add Goal'/>
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals...</Text>
+      </View>
+    </View>
+
+  );
+}
+
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 60,
+    paddingHorizontal: 16
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24, 
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey'
+  },
+  textInput: {
+    borderWidth: 1, 
+    borderColor: 'grey',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 5
+  }
+
+});
